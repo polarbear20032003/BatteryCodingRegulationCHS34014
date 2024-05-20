9fbbd9a0418acdc0126658987178dfb4e53212c7
DESKTOP-BJSE955\youy <baix_1111@yahoo.com>
2024-05-20 17:29:22 +0800

    增加了包类型函数，更改了包厂家函数

--- getpackinfo.py
+++ getpackinfo.py
@@ -14,6 +14,10 @@ def get_pack_factory(pack_name):
             for line in f:
                 packfac.append(line.strip().split(","))
 
+        f.close()
+    else:
+        return "Unknown Factory"
+
     if len(pack_name) == 26 or len(pack_name) == 24:
         packfaccod=pack_name[0:2]
         for i in packfac:
@@ -23,3 +27,14 @@ def get_pack_factory(pack_name):
     else:
         return "Unknown Factory"
 
+def get_pack_type(pack_name):
+    if len(pack_name) == 26 or len(pack_name) == 24:
+        if pack_name[3] == "P":
+            return "动力蓄电池包"
+        elif pack_name[3] == "M":
+            return "蓄电池模块"
+
+        elif pack_name[3] == "C":
+            return "单体蓄电池"
+        else:
+            return "未知类型"
